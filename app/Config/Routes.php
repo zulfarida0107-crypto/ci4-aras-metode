4ba70bf71ac92d56bc05249832cb05a3ea7f0d83
adminzulfa <zulfarida0107@gmail.com>
2026-05-29 14:08:30 +0700

    docs: penghapusan beberapa file View dan fix: perbaikan kode pada file debugbar

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -3,13 +3,9 @@
 use CodeIgniter\Router\RouteCollection;
 
 /** @var RouteCollection $routes */
-<<<<<<< HEAD
-$routes->get('/', 'Home::index');
-=======
 $routes->get('/', 'DashboardController::index');
 $routes->get('/dashboard', 'DashboardController::index');
 $routes->get('/responden', 'RespondenController::index');
 $routes->get('/eksperimen', 'EksperimenController::index');
 $routes->get('/panduan', 'PanduanController::index');
 $routes->get('/aras', 'ArasController::index');
->>>>>>> 30f665c (feat: implementasi UI/UX lengkap untuk semua halaman tab dan eksperimen mandiri)
